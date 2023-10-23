a69cec86bce7ff69995ab320d8df57ad990eaf7a
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-10-23 16:58:20 -0300

    first commit

---
+++ SpriteKitIntegration/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SpriteKitIntegration
+//
+//  Created by thaxz on 23/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpriteKitIntegration/SpriteKitIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitIntegrationApp.swift
+//  SpriteKitIntegration
+//
+//  Created by thaxz on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
