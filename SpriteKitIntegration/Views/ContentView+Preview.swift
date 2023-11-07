2a918a799e5d11e2c7b40f8a4664f10f9e257dee
Juliano Vaz <juliano@academy.cin.ufpe.br>
2023-11-07 09:08:53 -0300

    refactore view files :q :exit exit q

---
+++ SpriteKitIntegration/Views/ContentView+Preview.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView+Preview.swift
+//  SpriteKitIntegration
+//
+//  Created by Juliano on 07/11/23.
+//
+
+import SwiftUI
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
