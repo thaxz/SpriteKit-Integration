780b9a54cc1595eb9c37adb6e13edf8223476133
Juliano Vaz <juliano@academy.cin.ufpe.br>
2023-11-07 09:10:04 -0300

    refactore gamescene files

---
+++ SpriteKitIntegration/GameScene/GameScene+Constants.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene+Constants.swift
+//  SpriteKitIntegration
+//
+//  Created by Juliano on 07/11/23.
+//
+
+import UIKit.UIScreen
+
+extension GameScene {
+    static let height = UIScreen.main.bounds.height
+    static let width = UIScreen.main.bounds.width
+}
